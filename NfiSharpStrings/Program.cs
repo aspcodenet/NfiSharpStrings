9de95b4bbd8785bb7d17ab992d10f2280ebed164
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-05-12 11:04:34 +0200

    Add project files.

---
+++ NfiSharpStrings/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace NfiSharpStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string text = "All Kind OF LeTTeRs";
+                Console.WriteLine(text.ToLower());
+
+                string meddelande = Console.ReadLine();
+
+
+
+                if (meddelande.Length > 10)
+                {
+                    Console.WriteLine("Max 9 tecken");
+                }
+
+                if (meddelande.Contains("dum"))
+                {
+                    meddelande = meddelande.Replace("dum", "***");
+                }
+
+
+                Console.WriteLine($"Chatten: {meddelande}");
+            }
+
+            
+
+            //string namn = "stefan *** hej hej";
+
+
+
+
+            //if(namn.Equals())
+
+
+
+
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
